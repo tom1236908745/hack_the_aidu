cf891bb0d89b01a9b47a037fcc9236ab8f7cfb56
ryoma okuda <catontheskate4423@gmail.com>
2021-08-08 02:34:38 +0900

    huge fix of frontend

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,73 @@
 import Head from 'next/head'
 import { useEffect } from 'react'
 import { useState } from 'react'
-import { useQuery } from 'react-query'
+import { useMutation, useQuery } from 'react-query'
 import { field_get } from '../apis/field_get'
 import { subjects_get } from '../apis/subjects_get'
 import styles from '../styles/Home.module.css'
-import ButtonAppBar from '../components/ButtonAppBar'
 import SelectLabels from '../components/SelectLabels'
 import Header from '../components/Header'
+import ButtonAppBar from '../components/ButtonAppBar'
+import { Button } from '@material-ui/core'
+import { test_post } from '../apis/test_post'
+import { test_get } from '../apis/test_get'
+import { test_object } from '../interfaces/test_get_type'
+import { useRouter } from 'next/router'
+
+export const goRouter = () => {
+
+  const [goPath, setGoPath] = useState<string>(null)
+
+  const router = useRouter()
+  useEffect(() => {
+    if (goPath) {
+      router.push(goPath)
+      setGoPath(null)
+    }
+  }, [goPath])
+
+  return setGoPath
+}
+
 export default function Home() {
+
+  // states
 
   const [subject, setSubject] = useState<string[]>([])
   const [field, setField] = useState<string[]>([])
+  const types = ["期末", "中間", "対策", "レポート"]
 
   const [search__subject_data, setSearch_subject_data] = useState<string>("fu09")
   const [search__field_data, setSearch_field_data] = useState<string>("c1-c2")
+  const [search__type_data, setSearch_type_data] = useState<number>(0)
+
+  const [tests, setTest] = useState<any>([])
+
+  const setGo = goRouter()
+
+  // react query
 
   const { data: subjects_data, isLoading: subjects_isLoading } = useQuery('subjects_get', () =>
     subjects_get())
 
   const { data: field_data, isLoading: field_isLoading } = useQuery('field_get', () =>
     field_get())
+
+
+  const {
+    mutate: fetch_test_mutation,
+    isLoading: mutateIsLoading
+  } = useMutation((_) => test_get(search__subject_data, search__field_data, search__type_data), {
+    onSuccess: (res) => {
+      // // メインページに遷移
+      setTest(res)
+      console.log(res);
+
+    },
+    onError: (errorMessage: string) => {
+      alert("failed")
+    }
+  })
 
 
   useEffect(() => {
@@ -44,26 +91,115 @@ export default function Home() {
   }, [field_isLoading])
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>hack the aidu</title>
-        <meta name="description" content="examines examinations!" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header />
-      <main className={styles.main}>
 
-        <h1 className={styles.title}>
-          Welcome to hack the aidu
-        </h1>
-        <div>
+    <>
+      <ButtonAppBar />
+      <div className={styles.container}>
+        <Head>
+          <title>hack the aidu</title>
+          <meta name="description" content="examines examinations!" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-          <SelectLabels value={search__subject_data} items={subject} name="教科" onChange={(e) => setSearch_subject_data(e.target.value)} />
+        <main className={styles.main}>
+          <div className={styles.flexerow}>
 
-          <SelectLabels value={search__field_data} items={field} name="フィールド" onChange={(e) => setSearch_field_data(e.target.value)} />
+            <div className={styles.boxboxer}>
+              <div className={styles.flexercol}>
+                <div className={styles.textcenter}>
+                  <div className={styles.ml}>
+                    教科選択
+                  </div>
+                </div>
+                <SelectLabels value={search__subject_data} items={subject} name="教科" onChange={(e) => setSearch_subject_data(e.target.value)} />
+              </div>
+            </div>
 
-        </div>
-        {/* <h1 className={styles.title}>
+            <div className={styles.boxboxer}>
+              <div className={styles.flexercol}>
+                <div className={styles.textcenter}>
+                  <div className={styles.ml}>
+                    フィールド選択
+                  </div>
+                </div>
+                <SelectLabels value={search__field_data} items={field} name="フィールド" onChange={(e) => setSearch_field_data(e.target.value)} />
+              </div>
+            </div>
+
+            <div className={styles.boxboxer}>
+              <div className={styles.flexercol}>
+                <div className={styles.textcenter}>
+                  <div className={styles.ml}>
+                    種類選択
+                  </div>
+                </div>
+                <SelectLabels value={types[search__type_data]} items={types} name="種類" onChange={(e) => setSearch_type_data(types.indexOf(e.target.value))} />
+              </div>
+            </div>
+
+            <div className={styles.marg}>
+              <Button className={styles.clickme}>
+                <div
+                  className={styles.boxboxergo}
+                  onClick={() => {
+                    fetch_test_mutation()
+                    console.log(search__subject_data, search__field_data, search__type_data)
+                  }}>
+                  <div className={styles.flexercol}>
+                    <div className={styles.textcenter}>
+                      検索
+                    </div>
+                  </div>
+                </div>
+              </Button>
+            </div>
+          </div>
+
+
+          {
+            tests.map((obj: test_object, key: number) => {
+              return (
+                <div key={key} className={styles.boxboxer}>
+                  <div className={styles.flexercol}>
+                    <div className={styles.textcenter}>
+                      <div className={styles.ml}>
+                        {obj.nendo} {types[obj.kind]}
+                      </div>
+                    </div>
+
+                    <Button className={styles.clickme}>
+                      <div
+                        className={styles.boxboxergo2}
+                        onClick={() => {
+                          alert("新たなリンクを受け取ってtest_fix()する")
+                        }}>
+                        <div className={styles.flexercol}>
+                          <div className={styles.textcenter}>
+                            リンク更新
+                          </div>
+                        </div>
+                      </div>
+                    </Button>
+
+                    <Button className={styles.clickme}>
+                      <div
+                        className={styles.boxboxergo2}
+                        onClick={() => {
+                          setGo(obj.file_url)
+                        }}>
+                        <div className={styles.flexercol}>
+                          <div className={styles.textcenter}>
+                            遷移
+                          </div>
+                        </div>
+                      </div>
+                    </Button>
+                  </div>
+                </div>
+              )
+            })
+          }
+          {/* <h1 className={styles.title}>
           {
             subject.map((st: string, key: number) => {
               return (
@@ -81,8 +217,9 @@ export default function Home() {
             })
           }
         </h1> */}
-      </main>
+        </main>
 
-    </div>
+      </div>
+    </>
   )
 }
