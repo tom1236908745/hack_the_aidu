e02610d7d3e006dc2a7b5acb3d370d85577ffe7a
tom1236908745 <tom1233908745@gmail.com>
2021-08-14 01:55:12 +0900

    fix dateTable

--- components/DataTable.tsx
+++ components/DataTable.tsx
@@ -1,29 +1,29 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import EditDialog from '../components/EditDialog'
-import { Button } from '@material-ui/core';
-import { useMutation } from 'react-query';
-import { test_fix } from '../apis/test_fix';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import EditDialog from "../components/EditDialog";
+import { Button } from "@material-ui/core";
+import { useMutation } from "react-query";
+import { test_fix } from "../apis/test_fix";
 const useStyles = makeStyles({
   table: {
     minWidth: 650,
   },
   link: {
     color: "red",
-      '&:hover': {
-        color: "yellow",
-      },
-      '&:active': {
-        color: "green",
-      },
-  }
+    "&:hover": {
+      color: "yellow",
+    },
+    "&:active": {
+      color: "green",
+    },
+  },
 });
 
 // function createData(nendo: number, subject: string, field: string, kind: string, url: string, insert_data: string) {
@@ -40,75 +40,79 @@ const useStyles = makeStyles({
 // ];
 
 export interface data_table_type {
-
-  test: any,
-  fetch_test_mutation: any
+  test: any;
+  fetch_test_mutation: any;
 }
 
 export const DataTable = ({ test, fetch_test_mutation }: data_table_type) => {
-
-  const classes = useStyles()
+  const classes = useStyles();
 
   const {
     mutate: post_delete_test_mutation,
-    isLoading: mutateIsLoading
+    isLoading: mutateIsLoading,
   } = useMutation((data: any) => test_fix(data), {
     onSuccess: () => {
-      fetch_test_mutation()
+      fetch_test_mutation();
     },
-  })
+  });
 
   const handleSave = (test) => {
-    let new_test = Object.assign({}, test)
-    new_test.delete_flg = true
-    post_delete_test_mutation(new_test)
-  }
-
+    let new_test = Object.assign({}, test);
+    new_test.delete_flg = true;
+    post_delete_test_mutation(new_test);
+  };
 
   return (
     <TableContainer component={Paper}>
       <Table className={classes.table} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell align="right">年度</TableCell>
-            <TableCell align="right">更新日</TableCell>
-            <TableCell align="right">リンク</TableCell>
-
-
+            <TableCell align="center">年度</TableCell>
+            <TableCell align="center">更新日</TableCell>
+            <TableCell align="center">リンク</TableCell>
+            <TableCell align="center">編集</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {test.map((test) => {
-            const link = test.file_url.slice(0,30) + '...'
-            if (!test.delete_flg) return (
-              <TableRow key={test.insert_date}>
-                <TableCell align="right">{test.nendo}</TableCell>
-                <TableCell align="right">{test.insert_date.toString()}</TableCell>
-                <TableCell align="right">
-                  <Button className={classes.link}>
-                    <a href={test.file_url}>{link}</a>
-                  </Button>
-                </TableCell>
-                <TableCell align="right">
+            const timeStamp = test.insert_date.toDate();
+            const formatTime = `${timeStamp.getFullYear()}/${
+              timeStamp.getMonth() + 1
+            }/${timeStamp.getDate()}`;
+            const link = test.file_url.slice(0, 40) + "...";
+            if (!test.delete_flg)
+              return (
+                <TableRow key={test.insert_date}>
+                  <TableCell align="center">{test.nendo}</TableCell>
+                  <TableCell align="center">
+                    {formatTime}
+                  </TableCell>
+                  <TableCell align="center">
+                    <Button className={classes.link}>
+                      <a href={test.file_url}>{link}</a>
+                    </Button>
+                  </TableCell>
+                  <TableCell align="center">
+                    <Button
+                      onClick={() => {
+                        handleSave(test);
+                      }}
+                    >
+                      削除
+                    </Button>
 
-                  <Button onClick={() => {
-                    handleSave(test)
-                  }}>
-                    削除
-                  </Button>
-
-                  <EditDialog test={test} fetch_test_mutation={fetch_test_mutation} />
-
-                </TableCell>
-              </TableRow>
-            )
-          }
-          )}
+                    <EditDialog
+                      test={test}
+                      fetch_test_mutation={fetch_test_mutation}
+                    />
+                  </TableCell>
+                </TableRow>
+              );
+          })}
         </TableBody>
       </Table>
     </TableContainer>
   );
-}
+};
 
-
-export default DataTable
+export default DataTable;
